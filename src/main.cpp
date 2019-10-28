c551737cb35a1350250a1081158d378af787905a
Doug <dougsmcgirr@gmail.com>
2019-10-28 15:53:50 -0700

    added fps counter

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@
  4) Call getPosition() to get the vec3 of where the current calculated position is. 
  ***********************/
 
-#define VPLRESOLUTION 16
+#define VPLRESOLUTION 64
 
 #include <chrono>
 #include <iostream>
@@ -553,6 +553,9 @@ int main(int argc, char *argv[])
 
 		// convert microseconds (weird) to seconds (less weird)
 		deltaTime *= 0.000001;
+
+		// display framerate
+		cout << "FPS: " << 1.0/deltaTime << endl;
 
 		// reset lastTime so that we can calculate the deltaTime
 		// on the next frame
