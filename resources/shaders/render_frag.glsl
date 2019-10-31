206d5cc52a58d08eb638a11a3f7b35e6ae5d2399
Doug <dougsmcgirr@gmail.com>
2019-10-30 18:00:37 -0700

    more adjustments

--- resources/shaders/render_frag.glsl
+++ resources/shaders/render_frag.glsl
@@ -8,26 +8,22 @@ in vec3 fragNor;
 in vec3 fragPos;
 layout(location = 0) out vec4 color;
 
-#define VPLdistanceFalloffScalar 0.25
-#define VPLdistanceFalloffExponent 1.4
+#define VPLdistanceFalloffScalar 0.20
+#define VPLdistanceFalloffExponent 1.5
 #define VPLintensityScalar 60
 #define VPLbaseColorScalar 0.05
 #define VPLcolorScalar 0.95
 
 void main()
 {
-	float distanceScalar, diffuseScalar;
+	float distanceCoefficient, diffuseCoefficient;
 	vec4 currentVPLPos, currentVPLColor;
 	vec2 texCoords;
 
 	color = vec4(0,0,0,1);
 	vec3 normal = normalize(fragNor);
 
-	// regular diffuse lighting
-	//diffuseScalar = (dot(normal, normalize(vec3(0,-3,0) - fragPos)) + 0.5);
-	//color = vec4(diffuseScalar * vec3(1.0/distance(vec3(0,-3,0), fragPos)), 1);
-
-	 //light based on VPLs
+	// loop through all VPL's and "accumulate" light
 	for(int i = 0; i < VPLresolution; i++)
 	{
 		for(int j = 0; j < VPLresolution; j++)
@@ -36,9 +32,9 @@ void main()
 			currentVPLPos = (texture(VPLpositions, texCoords) - vec4(0.5)) * 50.0;
 			currentVPLColor = texture(VPLcolors, texCoords);
 
-			distanceScalar = clamp(VPLdistanceFalloffScalar / (pow(distance(fragPos, currentVPLPos.xyz), VPLdistanceFalloffExponent)), 0, 1);
-			diffuseScalar = clamp(dot(normalize(currentVPLPos.xyz - fragPos), normal) + 1.5 ,0,1);
-			color += vec4(((VPLcolorScalar*currentVPLColor.xyz + VPLbaseColorScalar*baseColor)),1) * diffuseScalar * distanceScalar;
+			distanceCoefficient = clamp(VPLdistanceFalloffScalar / (pow(distance(fragPos, currentVPLPos.xyz), VPLdistanceFalloffExponent)), 0, 1);
+			diffuseCoefficient = clamp(dot(normalize(currentVPLPos.xyz - fragPos), normal) + 1.5 ,0,1);
+			color += vec4(((VPLcolorScalar*currentVPLColor.xyz + VPLbaseColorScalar*baseColor)),1) * diffuseCoefficient * distanceCoefficient;
 			
 		}
 	}
--- resources/shaders/vpl_frag.glsl
+++ resources/shaders/vpl_frag.glsl
@@ -8,15 +8,16 @@ in vec3 fragNor;
 
 uniform vec3 lightPos;
 
+#define VPLdistanceFalloffScalar 0.25
+#define VPLdistanceFalloffExponent 0.70
+#define VPLintensityScalar 5
+
 void main()
 {
 	float diffuseCoefficient = clamp(dot(normalize(lightPos - fragPos), normalize(fragNor)), 0, 1);
-	float distanceScalar = 5.0 * clamp((1 / distance(lightPos, fragPos)), 0, 1);
-
-	vec4 diffuse = vec4(diffuseCoefficient * distanceScalar * baseColor, 1);
-	vec4 ambient = diffuse * 0.8;
+	float distanceCoefficient = VPLintensityScalar * clamp((1 / pow(distance(lightPos, fragPos), VPLdistanceFalloffExponent)), 0, 1);
 
 	// TODO plz fix
 	VPLpositions = vec4((fragPos/50.0) + vec3(.5,.5,.5), 1);
-	VPLcolors = vec4(diffuseCoefficient * distanceScalar * baseColor, 1);
+	VPLcolors = vec4(diffuseCoefficient * distanceCoefficient * baseColor, 1);
 }
