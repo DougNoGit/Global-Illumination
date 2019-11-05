b46bd36f3c456a7ffa2d97bf4b80c59f73b287ed
DougNoGit <dougsmcgirr@gmail.com>
2019-11-05 13:53:38 -0800

    used high precision floats to store VPL positions

--- src/main.cpp
+++ src/main.cpp
@@ -319,7 +319,7 @@ public:
 		// - position color buffer
 		glGenTextures(1, &VPLpositions);
 		glBindTexture(GL_TEXTURE_2D, VPLpositions);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, VPLpositions, 0);
