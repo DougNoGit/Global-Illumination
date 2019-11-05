9353025e9f0919eba3a3153d3c55e9c066f4afa5
Doug <dougsmcgirr@gmail.com>
2019-11-05 11:53:37 -0800

    changing light pos

--- src/main.cpp
+++ src/main.cpp
@@ -596,7 +596,7 @@ public:
 	{
 		auto Model = make_shared<MatrixStack>();
 		t += 0.25 * frametime;
-		lightPos = vec3(1 * sin(t), 5, 1 * cos(t) - 5);
+		lightPos = vec3(0.2 * sin(t), 6, 0.2 * cos(t));
 
 		// draw mesh
 		Model->pushMatrix();
